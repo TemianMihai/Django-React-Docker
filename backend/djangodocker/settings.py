5758f387b640839dd4e29cf511decbbfbf8934b0
Mihai Temian <mihai.temian@gmail.com>
2020-07-16 17:34:27 +0300

    deleting secret key

--- backend/djangodocker/settings.py
+++ backend/djangodocker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p6i5sp63ao11&k2fmb-ey6g#ko$q($dg2ryl=k4*-96)+u*&e&'
+SECRET_KEY = '' #YOUR SECRET KEY 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
